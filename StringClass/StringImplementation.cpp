47fbcb2b10b7e086fa62f277cc0cc9a0f3cba872
arunkumaradityan <adityanarunkumar@gmail.com>
2018-01-13 23:11:27 +0530

    CI by Arun
    Added Function - RemoveDuplicate()

--- StringClass/StringImplementation.cpp
+++ StringClass/StringImplementation.cpp
@@ -1,1 +1,14 @@
 #include "StringImplementation.h"
+
+//**************************************************
+/// Author		:	Arun Kr Adityan
+/// Function	:	RemoveDuplicate
+/// FullPath	:	CMyString::RemoveDuplicate
+/// AccessType	:	public
+/// ReturnType	:	char*
+/// Parameter	:	void
+/// Purpose		:	It removes the duplicate character
+//**************************************************
+char* CMyString::RemoveDuplicate()
+{
+}
--- StringClass/StringImplementation.h
+++ StringClass/StringImplementation.h
@@ -1,18 +1,31 @@
+#include <iostream>
+#include <conio.h>
 using namespace std;
 
 class CMyString
 {
 public:
-	// Constructor
+	// Constructor and Destructors
 	CMyString();
 	CMyString(char* strInput);
-
-	// Copy Constructor
-	CMyString(CMyString & obj);
-
-	// Destructor
 	~CMyString();
 
+	// Copy Constructor and Assignment Operator
+	CMyString(CMyString & obj);
+	CMyString& operator=(CMyString & obj);
+
+	//**************************************************
+	/// Author		:	Arun Kr Adityan
+	/// Function	:	RemoveDuplicate
+	/// FullPath	:	CMyString::RemoveDuplicate
+	/// AccessType	:	public
+	/// ReturnType	:	char*
+	/// Parameter	:	void
+	/// Purpose		:	It removes the duplicate character
+	//**************************************************
+	char* RemoveDuplicate();
+
+	// Private Data Members
 private:
 	char* m_strInput;
 };
