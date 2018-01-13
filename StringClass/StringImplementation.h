c78c0166da55b249b574aea7c8ea31c97190168b
arunkumaradityan <35401769+arunkumaradityan@users.noreply.github.com>
2018-01-13 21:54:43 +0530

    Add files via upload

--- StringClass/StringImplementation.h
+++ StringClass/StringImplementation.h
@@ -2,4 +2,14 @@ using namespace std;
 
 class CMyString
 {
+public:
+	// Constructor
+	CMyString();
+	CMyString(char* strInput);
+
+	// Destructor
+	~CMyString();
+
+private:
+	char* m_strInput;
 };
